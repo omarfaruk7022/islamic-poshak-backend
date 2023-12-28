5180c0eb76cf606f6cced1a3df68da410678bc3b
omar-faruk-bdtask <omarfaruk7022@gmail.com>
2023-12-28 10:44:02 +0600

    minor changes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello BMW!");
+  res.send("Hello World!");
 });
 
 const productRoute = require("./routes/product.route");
